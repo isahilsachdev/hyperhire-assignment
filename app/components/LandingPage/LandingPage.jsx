c7378fa3b8139410346b5001f7fe5f8b90d8b66a
Sahil Sachdev <isahilsachdev00@gmail.com>
2024-10-23 00:32:28 +0530

    initial changes

---
+++ app/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <p>LandingPage</p>
+  )
+}
+
+export default LandingPage
---
+++ app/landing-page/page.jsx
@@ -0,0 +1,11 @@
+'use client';
+
+import LandingPage from "../components/LandingPage/LandingPage";
+
+export default function Home() {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  );
+}
